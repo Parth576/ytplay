43715807adfeee7dd365e4c6c4439b309e4227df
Parth <parthshah576@gmail.com>
2021-09-19 19:15:37 +0530

    some bug fixes/integrating audio lib

--- audio/audio.go
+++ audio/audio.go
@@ -11,11 +11,22 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-func printTime(streamer beep.StreamSeekCloser, format beep.Format) {
+func printTime(streamer beep.StreamSeekCloser, format beep.Format, songFinished chan bool) {
+    onesecCtr := 0
 	for {
 		select {
 		case <-time.After(time.Second):
-			fmt.Println(format.SampleRate.D(streamer.Position()).Round(time.Second))
+            currTime := format.SampleRate.D(streamer.Position()).Round(time.Second)
+            var d time.Duration = 1*time.Second
+            if currTime == d {
+                onesecCtr++
+            }
+            if onesecCtr == 2 {
+                songFinished <- true
+                fmt.Printf("\r")
+                os.Exit(1)
+            }
+			fmt.Printf("\r%s/%s",format.SampleRate.D(streamer.Position()).Round(time.Second),format.SampleRate.D(streamer.Len()).Round(time.Second))
 		}
 
 	}
@@ -34,19 +45,32 @@ func Play(fp string) {
 
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
+    songFinished := make(chan bool, 1)
+
 	ctrl := &beep.Ctrl{Streamer: beep.Loop(-1, streamer), Paused: false}
 	speaker.Play(ctrl, beep.Callback(func() {
-		go printTime(streamer, format)
+		go printTime(streamer, format, songFinished)
 	}))
 
-	fmt.Println(format.SampleRate.D(streamer.Len()).Round(time.Second))
-
-	fmt.Print("Press any key to pause/resume. ")
-	for {
-		fmt.Scanln()
-
-		speaker.Lock()
-		ctrl.Paused = !ctrl.Paused
-		speaker.Unlock()
-	}
+	fmt.Println("Press ENTER to pause/resume. ")
+    // correct way to do channels but scanln blocks main thread so not channel input blocked as well
+	//for {
+    //    select {
+    //    case <- songFinished:
+    //        fmt.Println("song fin")
+    //        speaker.Close()
+    //        os.Exit(1)
+    //    default: 
+    //        fmt.Scanln()
+    //        speaker.Lock()
+    //        ctrl.Paused = !ctrl.Paused
+    //        speaker.Unlock()
+    //	}
+    //}
+    for {
+        fmt.Scanln()
+        speaker.Lock()
+        ctrl.Paused = !ctrl.Paused
+        speaker.Unlock()
+    }
 }
